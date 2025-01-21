e66352824f0ba4cd91ee67a924338d99682d43f0
bdBel <wildvolley22@gmail.com>
2025-01-21 17:10:59 -0500

    Add project files.

---
+++ BlazorCookbookApp.Client/Program.cs
@@ -0,0 +1,9 @@
+//Purpose of this app : Creating your first basic component 7
+//For our first component, we’ll create a Ticket component that renders a tariff name and a price
+//when the user navigates to a page
+
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+await builder.Build().RunAsync();
